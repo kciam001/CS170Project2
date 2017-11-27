b29771ca995419f0063af8513beef867dea8568e
kciam001 <kciam001@ucr.edu>
2017-11-26 16:52:14 -0800

    Initial Commit
    Added ability to read smallData file into a numpy

---
+++ proj2.py
@@ -0,0 +1,16 @@
+import math
+import struct
+import numpy as np
+
+def main():
+
+
+	smallDataName = 'CS170Smalltestdata__68.txt'
+	smallData = np.genfromtxt(smallDataName)
+	
+
+
+
+
+if __name__ == "__main__":
+	main()
